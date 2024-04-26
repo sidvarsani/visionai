e047e2369f2c1eb71885e9f506265b2241005984
siddharth <117082058+sidvarsani@users.noreply.github.com>
2024-04-26 20:05:33 +0530

    Add files via upload

--- vision.py
+++ vision.py
@@ -30,7 +30,7 @@ def get_gemini_response(input,image):
 
 ##initialize our streamlit app
 
-st.set_page_config(page_title="Gemini Image Demo")
+st.set_page_config(page_title="VisionAI")
 
 st.header("Gemini Application")
 input=st.text_input("Input Prompt: ",key="input")
